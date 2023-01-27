96d6cbc6c1f554bf349c034782b4134652c898b2
Jan Sůra <info@jsuradesign.cz>
2023-01-27 22:23:40 +0100

    Importing bootstrap js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,19 @@
 // Import scss file
 import '../scss/style.scss'
 
-// Import components
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// Or individual bootstrap imports
+// import Alert from 'bootstrap/js/dist/alert';
+// or, specify which plugins you need:
+// import { Tooltip, Toast, Popover } from 'bootstrap';
+
+// Import custom components
 import Foo from './components/Foo';
 
-// Init
+// Init components on document loaded
 window.onload = () => {
+    // Init Foo component
     Foo("Foo imported successfully");
 }
